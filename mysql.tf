5363b9f3cebcb62aa2b078d2d5e763cb96a8d9d4
mvpc-11232 <mvp2612@hotmail.com>
2022-12-15 08:34:31 +0530

    Auto Push Commit

--- mysql.tf
+++ mysql.tf
@@ -1,12 +1,11 @@
 resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
-  db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
+  username             = "admin1"
+  password             = "RoboShop1"
+  parameter_group_name = ????
   skip_final_snapshot  = true
 }
 
