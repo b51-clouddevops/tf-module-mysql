e1dcc7eb8ef6abeec5baa7902114a51102b458a6
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 08:17:05 +0530

    Auto Push Commit

--- mysql.tf
+++ mysql.tf
@@ -1,14 +1,14 @@
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "admin1"
-  password             = "RoboShop1"
-  parameter_group_name = aws_db_parameter_group.mysql-pg.name
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.mysql-subnet-grp.name
-  vpc_security_group  = [aws_security_group.allows_mysql.id] 
+  allocated_storage       = 10
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t3.micro"
+  username                = "admin1"
+  password                = "RoboShop1"
+  parameter_group_name    = aws_db_parameter_group.mysql-pg.name
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_db_subnet_group.mysql-subnet-grp.name
+  vpc_security_group_ids  = [aws_security_group.allows_mysql.id] 
 }
 
 
