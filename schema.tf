186517e026d12b888e8caf1d6c4b60a781fa0e1a
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 07:46:25 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -2,9 +2,10 @@ resource "null_resource" "schema" {
   provisioner "local-exec" {
     command = <<EOF 
 cd /tmp 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-unzip -o mongodb.zip 
-cd mongodn-main 
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+unzip -o mysql.zip 
+cd mysql-main 
+
 
 
 EOF 
