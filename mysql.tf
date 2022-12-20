1df52b3183a4604cab1d32303150186d909d8f93
mvpc-11232 <mvp2612@hotmail.com>
2022-12-20 08:27:53 +0530

    Auto Push Commit

--- mysql.tf
+++ mysql.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "mysql" {
-  # identifier            = "raoboshop-mysql-${var.ENV}" 
+  # identifier            = "roboshop-mysql-${var.ENV}" 
   allocated_storage       = 10
   engine                  = "mysql"
   engine_version          = "5.7"
